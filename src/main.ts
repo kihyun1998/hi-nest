8336398136c340e8d562a76ae109d132748ebb06
박기현 <kihyun1998@naver.com>
2024-07-08 16:33:09 +0900

    start study

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+/// 시작 지점
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
